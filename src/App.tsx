46f787718dae2d258e578a59c49a001963653c6a
Patrick Garcia <patrickvicentegarcia@gmail.com>
2023-09-01 23:58:52 -0300

    feat: adjustments in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
+import { useState } from "react";
 
+import { Header } from "./components/Header";
+import { NewTask } from "./components/NewTask";
+import { Tasks } from "./components/Task";
 
-import './global.css';
+import styles from "./App.module.css";
 
+type Task = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+};
 
-function App() {
+function useTasks(initialTasks: Task[] = []) {
+  const [tasks, setTasks] = useState<Task[]>(initialTasks);
 
-  return (
-    <h1>Hello testing</h1>
-  )
+  function addNewTask(newTaskTitle: string) {
+    const id = new Date().getTime().toString();
+    const newTask: Task = { id, title: newTaskTitle, isCompleted: false };
+    setTasks((state) => [...state, newTask]);
+  }
+
+  function removeTask(taskId: string) {
+    setTasks((state) => {
+      return state.filter((taskItem) => taskItem.id !== taskId);
+    });
+  }
+
+  function toggleStateCompleteTask(taskId: string) {
+    setTasks((state) => {
+      return state.map((taskItem) => {
+        if (taskItem.id === taskId) {
+          return {
+            ...taskItem,
+            isCompleted: !taskItem.isCompleted,
+          };
+        }
+        return taskItem;
+      });
+    });
+  }
+
+  return { tasks, addNewTask, removeTask, toggleStateCompleteTask };
 }
 
-export default App
+export function App() {
+  const { tasks, addNewTask, removeTask, toggleStateCompleteTask } = useTasks();
+
+  return (
+    <div className={styles.app}>
+      <Header />
+      <NewTask onCreateNewTask={addNewTask} />
+      <Tasks
+        onCompleteTask={toggleStateCompleteTask}
+        onRemoveTask={removeTask}
+        tasks={tasks}
+      />
+    </div>
+  );
+}
